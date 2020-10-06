5ca3e430831066f6942942c85d2cae5ea38e9f8e
Anton <toshi4ka7@mail.ru>
2020-10-06 22:59:27 +0300

    feat: ready task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    resArray = [];
+    for (let i = 0; i < expr.length; i += 10) {
+      resArray.push(expr.substring(i, i + 10));
+    }
+
+    resArray.forEach(function(value, index, array) {
+      array[index] = code(value.replace(/^0+/g, ""));
+    });
+
+    function code(str) {
+
+      if (str === '**********') return ' ';
+
+      let resArray = [];
+      for (let i = 0; i < str.length; i += 2) {
+        resArray.push(str.substring(i, i + 2));
+      }
+
+      let resStr = '';
+      resArray.forEach(function(value) {
+        if (value === '10') {
+           resStr += '.';
+        } else if (value === '11') {
+          resStr += '-';
+        }
+      })
+      
+      return MORSE_TABLE[resStr];
+    }
+
+
+    return resArray.join('');
 }
 
 module.exports = {
